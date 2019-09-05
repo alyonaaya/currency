b78fa870ee0e8f6c6e6f24f3f745a523db021fa0
aayarulina <voronina.vt@gmail.com>
2019-09-05 08:59:48 +0300

    UTC time in container

--- boot.sh
+++ boot.sh
@@ -5,7 +5,7 @@ mkdir dbdata
 flask db upgrade
 
 venv/bin/python currency_get.py
-echo '0 15 * * * /app/venv/bin/python /app/currency_get.py' >> /etc/crontabs/root
+echo '0 12 * * * /app/venv/bin/python /app/currency_get.py' >> /etc/crontabs/root
 crond -b
 
 exec gunicorn -b :5000 --access-logfile - --error-logfile - currency_show:app
